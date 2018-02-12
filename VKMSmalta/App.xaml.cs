994213cf50dde71759f0dc09d0ed78bdeafd4604
Данила Червонный <webmaster.pico@gmail.com>
2018-02-12 22:01:59 +0300

    Исправление ошибок

--- VKMSmalta/App.xaml.cs
+++ VKMSmalta/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DevExpress.Xpf.Core;
 
 namespace VKMSmalta
 {
@@ -13,5 +14,9 @@ namespace VKMSmalta
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            ApplicationThemeHelper.ApplicationThemeName = "Office2016White";
+        }
     }
 }
