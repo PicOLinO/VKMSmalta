82f07899d54b4f0c2dcb77dc1c963e0fabcb632d
Данила Червонный <webmaster.pico@gmail.com>
2018-02-20 00:07:27 +0300

    Каркас онлайн - сервиса

---
+++ VKMSmalta/Domain/ExamineResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace VKMSmalta.Domain
+{
+    public class ExamineResult
+    {
+        public int Value { get; set; }
+        public List<Action> Actions { get; set; }
+        public Algorithm Algorithm { get; set; }
+    }
+}
--- VKMSmalta/Services/NetworkService.cs
+++ VKMSmalta/Services/NetworkService.cs
@@ -1,7 +1,40 @@
-﻿namespace VKMSmalta.Services
+﻿using System;
+using VKMSmalta.Domain;
+
+namespace VKMSmalta.Services
 {
-    public class NetworkService : ServiceBase<NetworkService>
+    public class NetworkService
     {
-        
+        private string user;
+        private string password;
+
+        private string adminMachineUrl;
+
+        public static NetworkService Instance { get; protected set; }
+
+        public static void InitializeService(string user, string password, string adminMachineUrl)
+        {
+            if (Instance == null)
+            {
+                Instance = new NetworkService(user, password, adminMachineUrl);
+            }
+        }
+
+        public NetworkService(string user, string password, string adminMachineUrl)
+        {
+            this.user = user;
+            this.password = password;
+            this.adminMachineUrl = adminMachineUrl;
+        }
+
+        public void Authorize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendExamineResultToAdmin(ExamineResult examineResult)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
