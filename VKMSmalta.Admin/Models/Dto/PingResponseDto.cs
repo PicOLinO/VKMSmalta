ee9cd516cb26c143fe6f66e44dc57698ffdfa31a
Данила Червонный <webmaster.pico@gmail.com>
2018-03-06 02:25:58 +0300

    Реализация пинга

---
+++ VKMSmalta.Admin/Models/Dto/PingResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VKMSmalta.Admin.Models.Dto
+{
+    public class PingResponseDto
+    {
+        public string ServiceVersion { get; set; }
+        public TimeSpan Uptime { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(ServiceVersion)}: {ServiceVersion}, {nameof(Uptime)}: {Uptime}";
+        }
+    }
+}
