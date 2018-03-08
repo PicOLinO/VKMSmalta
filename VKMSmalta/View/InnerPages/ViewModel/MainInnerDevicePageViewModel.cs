f0939d77940289ee661ac213f8de3a9d353d007a
Данила Червонный <webmaster.pico@gmail.com>
2018-03-09 01:39:43 +0300

    рефакторинг

--- VKMSmalta/View/InnerPages/ViewModel/MainInnerDevicePageViewModel.cs
+++ VKMSmalta/View/InnerPages/ViewModel/MainInnerDevicePageViewModel.cs
@@ -27,64 +27,64 @@ namespace VKMSmalta.View.ViewModel
             Elements = new ObservableCollection<ElementViewModelBase>
                        {
                            //Тумблеры в середине
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_1channel").WithStartupRotation(90).At(280,341).Thumbler(historyService).WithDependencyAction(GiveMe.DependencyAction().WithDependencyElementName("main_reciever_1channel").WithDefaultDependencyValues(DependencyActionsDefaultValues.OneToOneEqualsTwoCount).Please()).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_2channel").WithStartupRotation(90).At(323,340).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_3channel").WithStartupRotation(90).At(367,341).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_4channel").WithStartupRotation(90).At(411,341).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_1channel").WithStartupRotation(90).At(280,341).Thumbler(historyService).WithDependencyAction(GiveMe.DependencyAction().WithDependencyElementName("main_reciever_1channel").WithDefaultDependencyValues(DependencyActionsDefaultValues.OneToOneEqualsTwoCount).Please()).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_2channel").WithStartupRotation(90).At(323,340).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_3channel").WithStartupRotation(90).At(367,341).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_4channel").WithStartupRotation(90).At(411,341).Thumbler(historyService).Please(),
 
                            //Подсветка в середине
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_reciever_1channel").At(309,540).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_reciever_2channel").At(353,540).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_reciever_3channel").At(396,540).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_reciever_4channel").At(441,541).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_antenna_leftside").At(483,540).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_reciever_1channel").At(309,540).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_reciever_2channel").At(353,540).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_reciever_3channel").At(396,540).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_reciever_4channel").At(441,541).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_antenna_leftside").At(483,540).LightBox().Please(),
 
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_transmitter_1channel").At(309,628).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_transmitter_2channel").At(353,628).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_transmitter_3channel").At(396,628).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_transmitter_4channel").At(441,627).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_antenna_rightside").At(484,627).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_transmitter_1channel").At(309,628).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_transmitter_2channel").At(353,628).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_transmitter_3channel").At(396,628).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_transmitter_4channel").At(441,627).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_antenna_rightside").At(484,627).LightBox().Please(),
 
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_defect_1channel").At(311,715).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_defect_2channel").At(354,715).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_defect_3channel").At(396,714).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_defect_4channel").At(441,714).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_1cooler").At(484,714).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_defect_1channel").At(311,715).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_defect_2channel").At(354,715).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_defect_3channel").At(396,714).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_defect_4channel").At(441,714).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_1cooler").At(484,714).LightBox().Please(),
 
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_glow_on").At(310,802).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_simulator").At(353,802).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_modulation").At(441,801).LightBox().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_2cooler").At(484,801).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_glow_on").At(310,802).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_simulator").At(353,802).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_modulation").At(441,801).LightBox().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_2cooler").At(484,801).LightBox().Please(),
 
                            //Тумблеры снизу
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(1).WithName("main_thumbler_simulator").At(582,385).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_antenna_leftside").At(584,662).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_antenna_rightside").At(586,752).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_light").At(656,753).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(1).WithName("main_thumbler_simulator").At(582,385).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_antenna_leftside").At(584,662).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_antenna_rightside").At(586,752).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_light").At(656,753).Thumbler(historyService).Please(),
                            
                            //Тумблеры справа сверху
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_power").At(68,1178).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_cold").At(67,1226).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_autosarpp").At(69,1278).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_aircontrol").At(70,1329).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_power").At(68,1178).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_cold").At(67,1226).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_autosarpp").At(69,1278).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_aircontrol").At(70,1329).Thumbler(historyService).Please(),
                            
                            //Тумблеры справа снизу
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_cooler").At(636,1170).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_light_maintance").At(637,1217).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_light_advanced").At(635,1262).Thumbler(historyService).Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_thumbler_light_table").At(636,1308).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_cooler").At(636,1170).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_light_maintance").At(637,1217).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_light_advanced").At(635,1262).Thumbler(historyService).Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_thumbler_light_table").At(636,1308).Thumbler(historyService).Please(),
 
                            //Стрелки слева
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_reception_1channel_arrow").WithStartupRotation(35).At(96,127).LittleArrow().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_reception_2channel_arrow").WithStartupRotation(35).At(288,127).LittleArrow().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_reception_3channel_arrow").WithStartupRotation(35).At(478,128).LittleArrow().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_reception_4channel_arrow").WithStartupRotation(35).At(665,128).LittleArrow().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_reception_1channel_arrow").WithStartupRotation(35).At(96,127).LittleArrow().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_reception_2channel_arrow").WithStartupRotation(35).At(288,127).LittleArrow().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_reception_3channel_arrow").WithStartupRotation(35).At(478,128).LittleArrow().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_reception_4channel_arrow").WithStartupRotation(35).At(665,128).LittleArrow().Please(),
 
                            //Стрелки справа
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_transmitter_1channel_arrow").WithStartupRotation(35).At(103,1004).LittleArrow().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_transmitter_2channel_arrow").WithStartupRotation(35).At(294,1004).LittleArrow().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_transmitter_3channel_arrow").WithStartupRotation(35).At(479,1003).LittleArrow().Please(),
-                           GiveMe.Element().On(InnerRegionPages.L001P).WithValue(0).WithName("main_transmitter_4channel_arrow").WithStartupRotation(35).At(667,1001).LittleArrow().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_transmitter_1channel_arrow").WithStartupRotation(35).At(103,1004).LittleArrow().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_transmitter_2channel_arrow").WithStartupRotation(35).At(294,1004).LittleArrow().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_transmitter_3channel_arrow").WithStartupRotation(35).At(479,1003).LittleArrow().Please(),
+                           GiveMe.Element().On(PageKey).WithValue(0).WithName("main_transmitter_4channel_arrow").WithStartupRotation(35).At(667,1001).LittleArrow().Please(),
                        };
         }
     }
