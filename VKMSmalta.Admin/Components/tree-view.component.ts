e3cb74c2b7cc1009555a13db2aaf40d526702874
Данила Червонный <webmaster.pico@gmail.com>
2018-03-09 11:24:50 +0300

    Добавление npm бовера и галпа

---
+++ VKMSmalta.Admin/Components/tree-view.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component } from '@angular/core';
+import { TreeView } from './tree-view.directive';
+import { ProjectRoleService } from '../../services/project-role.service';
+import swal from 'sweetalert2';
+
+@Component({
+    selector: 'tree-view-menu',
+    template: '<tree-view [menuList]="menuList"></tree-view>',
+    styleUrls: ['./tree-view.css']
+})
+export class TreeViewComponent {
+    public roleName: string;
+    menuList: any;
+    constructor(private _projectService: ProjectRoleService) {
+    }
+    ngOnInit() {
+        this.roleName = "Admin";
+        this._projectService.getMenuDetails(this.roleName).then((res: any) => {
+            this.menuList = res;
+        }, (error) => {
+            swal("Failed to get Treeview menu details", error._body, "error");
+        });
+    }
+}  
