5078eab3875574cd32be6225f52162f5fe907183
Данила Червонный <webmaster.pico@gmail.com>
2018-03-11 16:13:40 +0300

    Добавление новых тумблеров на новую страницу. рефакторинг имен

--- VKMSmalta/Dialogs/Factories/AlgorithmsFactory.cs
+++ VKMSmalta/Dialogs/Factories/AlgorithmsFactory.cs
@@ -25,22 +25,24 @@ namespace VKMSmalta.Dialogs.Factories
 
             var endStateOfElements = new Dictionary<string, int>
                                      {
-                                         {"main_thumbler_light", 1},
-                                         {"main_thumbler_1channel", 1},
-                                         {"main_thumbler_2channel", 1},
-                                         {"main_thumbler_3channel", 1},
-                                         {"main_thumbler_4channel", 1},
-                                         {"main_thumbler_simulator", 0},
-                                         {"adv_reciever_1channel", 1},
-                                         {"adv_reciever_2channel", 1},
-                                         {"adv_reciever_3channel", 1},
-                                         {"adv_reciever_4channel", 1},
-                                         {"adv_transmitter_1channel", 1},
-                                         {"adv_transmitter_2channel", 1},
-                                         {"adv_transmitter_3channel", 1},
-                                         {"adv_transmitter_4channel", 1},
-                                         {"adv_antenna_equal", 1},
-                                         {"adv_modulation", 0},
+                                         {"l001p_thumbler_light", 1},
+                                         {"l001p_thumbler_1channel", 1},
+                                         {"l001p_thumbler_2channel", 1},
+                                         {"l001p_thumbler_3channel", 1},
+                                         {"l001p_thumbler_4channel", 1},
+                                         {"l001p_thumbler_simulator", 0},
+                                         {"l001r_reciever_1channel", 1},
+                                         {"l001r_reciever_2channel", 1},
+                                         {"l001r_reciever_3channel", 1},
+                                         {"l001r_reciever_4channel", 1},
+                                         {"l001r_transmitter_1channel", 1},
+                                         {"l001r_transmitter_2channel", 1},
+                                         {"l001r_transmitter_3channel", 1},
+                                         {"l001r_transmitter_4channel", 1},
+                                         {"l001r_antenna_equal", 1},
+                                         {"l001r_modulation", 0},
+                                         {"l001k_modulation_13channel", 1},
+                                         {"l001k_modulation_24channel", 1},
                                      };
 
             var newAlgorithm = new Algorithm(startStateOfElements, endStateOfElements)
@@ -48,27 +50,30 @@ namespace VKMSmalta.Dialogs.Factories
                                    Name = "Подготовка изделия Л001 к включению",
                                    Actions = new LinkedList<Action>(new[]
                                                                     {
-                                                                        actionsFactory.GetClickAction("main_thumbler_light", "Установите данный тумблер в положение ПОДСВЕТ", 1),
+                                                                        actionsFactory.GetClickAction("l001p_thumbler_light", "Установите данный тумблер в положение ПОДСВЕТ", 1),
 
-                                                                        actionsFactory.GetClickAction("main_thumbler_1channel", "Установите данный тумблер в положение I КАНАЛ", 1),
-                                                                        actionsFactory.GetClickAction("main_thumbler_2channel", "Установите данный тумблер в положение II КАНАЛ", 1),
-                                                                        actionsFactory.GetClickAction("main_thumbler_3channel", "Установите данный тумблер в положение III КАНАЛ", 1),
-                                                                        actionsFactory.GetClickAction("main_thumbler_4channel", "Установите данный тумблер в положение IV КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001p_thumbler_1channel", "Установите данный тумблер в положение I КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001p_thumbler_2channel", "Установите данный тумблер в положение II КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001p_thumbler_3channel", "Установите данный тумблер в положение III КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001p_thumbler_4channel", "Установите данный тумблер в положение IV КАНАЛ", 1),
 
-                                                                        actionsFactory.GetClickAction("main_thumbler_simulator", "Установите данный тумблер в положение ОТКЛ.", 0),
+                                                                        actionsFactory.GetClickAction("l001p_thumbler_simulator", "Установите данный тумблер в положение ОТКЛ.", 0),
                                                                         
-                                                                        actionsFactory.GetClickAction("adv_reciever_1channel", "Установите данный тумблер в положение I КАНАЛ", 1),
-                                                                        actionsFactory.GetClickAction("adv_reciever_2channel", "Установите данный тумблер в положение II КАНАЛ", 1),
-                                                                        actionsFactory.GetClickAction("adv_reciever_3channel", "Установите данный тумблер в положение III КАНАЛ", 1),
-                                                                        actionsFactory.GetClickAction("adv_reciever_4channel", "Установите данный тумблер в положение IV КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001r_reciever_1channel", "Установите данный тумблер в положение I КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001r_reciever_2channel", "Установите данный тумблер в положение II КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001r_reciever_3channel", "Установите данный тумблер в положение III КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001r_reciever_4channel", "Установите данный тумблер в положение IV КАНАЛ", 1),
 
-                                                                        actionsFactory.GetClickAction("adv_transmitter_1channel", "Установите данный тумблер в положение I КАНАЛ", 1),
-                                                                        actionsFactory.GetClickAction("adv_transmitter_2channel", "Установите данный тумблер в положение II КАНАЛ", 1),
-                                                                        actionsFactory.GetClickAction("adv_transmitter_3channel", "Установите данный тумблер в положение III КАНАЛ", 1),
-                                                                        actionsFactory.GetClickAction("adv_transmitter_4channel", "Установите данный тумблер в положение IV КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001r_transmitter_1channel", "Установите данный тумблер в положение I КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001r_transmitter_2channel", "Установите данный тумблер в положение II КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001r_transmitter_3channel", "Установите данный тумблер в положение III КАНАЛ", 1),
+                                                                        actionsFactory.GetClickAction("l001r_transmitter_4channel", "Установите данный тумблер в положение IV КАНАЛ", 1),
 
-                                                                        actionsFactory.GetClickAction("adv_antenna_equal", "Установите данный тумблер в положение Эквивалент", 1),
-                                                                        actionsFactory.GetClickAction("adv_modulation", "Установите данный тумблер в положение ОТКЛ.", 0),
+                                                                        actionsFactory.GetClickAction("l001r_antenna_equal", "Установите данный тумблер в положение Эквивалент", 1),
+                                                                        actionsFactory.GetClickAction("l001r_modulation", "Установите данный тумблер в положение ОТКЛ.", 0),
+
+                                                                        actionsFactory.GetClickAction("l001k_modulation_13channel", "Установите данный тумблер в положение ВКЛ.", 1),
+                                                                        actionsFactory.GetClickAction("l001k_modulation_24channel", "Установите данный тумблер в положение ВКЛ.", 1),
                                                                     })
                                };
 
