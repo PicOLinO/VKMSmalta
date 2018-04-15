cea54e1df5515f03eae53bfe2e10b5e5d06cd843
Данила Червонный <webmaster.pico@gmail.com>
2018-04-15 10:40:59 +0300

    Добавление возможности произвести логин

--- VKMSmalta/Dialogs/ViewModel/LoginDialogViewModel.cs
+++ VKMSmalta/Dialogs/ViewModel/LoginDialogViewModel.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Security;
+using System.Security.Authentication;
+using System.Threading.Tasks;
 using DevExpress.Mvvm;
+using Newtonsoft.Json;
 
 namespace VKMSmalta.Dialogs.ViewModel
 {
     public class LoginDialogViewModel : DialogViewModelBase
     {
         private readonly IPasswordSupplier passwordSupplier;
+        private readonly string authorizeUri;
 
         public string Login
         {
@@ -19,22 +25,36 @@ namespace VKMSmalta.Dialogs.ViewModel
             get { return GetProperty(() => passwordSupplier.GetPassword()); }
         }
 
-        public DelegateCommand ClickCommand { get; set; }
+        public AsyncCommand ClickCommand { get; set; }
 
-        public LoginDialogViewModel(IPasswordSupplier passwordSupplier)
+        public LoginDialogViewModel(IPasswordSupplier passwordSupplier, string authorizeUri)
         {
             this.passwordSupplier = passwordSupplier;
+            this.authorizeUri = authorizeUri;
             CreateCommands();
         }
 
         private void CreateCommands()
         {
-            ClickCommand = new DelegateCommand(OnClick);
+            ClickCommand = new AsyncCommand(OnClick);
         }
 
-        private void OnClick()
+        private async Task OnClick()
         {
-            throw new NotImplementedException();
+            using (var httpClient = new HttpClient())
+            {
+                var json = JsonConvert.SerializeObject(new { Login = Login, Password = Password });
+                var body = new StringContent(json);
+                var response = await httpClient.PostAsync(authorizeUri, body);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    //TODO: Логин успешен
+                }
+                else
+                {
+                    throw new AuthenticationException("Неверный логин или пароль");
+                }
+            }
         }
     }
 }
