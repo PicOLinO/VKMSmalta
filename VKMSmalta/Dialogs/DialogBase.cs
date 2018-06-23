0045c156b40f1c945fa87ad20bc5461f7c243be7
PicOLinO <webmaster.pico@gmail.com>
2018-06-23 19:00:04 +0300

    Доработка диалогов

--- VKMSmalta/Dialogs/DialogBase.cs
+++ VKMSmalta/Dialogs/DialogBase.cs
@@ -1,6 +1,8 @@
 ﻿#region Usings
 
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 using DevExpress.Mvvm;
 using VKMSmalta.Dialogs.ViewModel;
 
@@ -10,12 +12,24 @@ namespace VKMSmalta.Dialogs
 {
     public class DialogBase : Window
     {
+        protected DialogBase()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            if (GetTemplateChild("closeButton") is Button closeButton)
+                closeButton.Click += (sender, args) => Close();
+            base.OnApplyTemplate();
+        }
+
         protected virtual void CreateCommands()
         {
             var vm = (DialogViewModelBase)DataContext;
             vm.CloseCommand = new DelegateCommand<bool?>(OnClosing);
         }
-        
+
         private void OnClosing(bool? parameter = null)
         {
             DialogResult = parameter;
