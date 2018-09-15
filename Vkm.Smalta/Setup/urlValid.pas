a6e7f8c861ef20c2fa74a8a9afdb3dfac9e92c81
Данила Червонный <webmaster.pico@gmail.com>
2018-09-15 13:28:01 +0300

    Доработка инсталлятора

---
+++ Vkm.Smalta/Setup/urlValid.pas
@@ -0,0 +1,37 @@
+﻿function ValidateUrl(url: string): boolean;
+
+var isHttps : boolean;
+
+begin
+	url := Uppercase(url);
+
+	if Length(url) >= 5 then
+	begin
+		isHttps := url[5] = 'S';
+	end;
+
+	if isHttps then
+	begin
+		Result :=
+		(Length(url) > 8) and
+		(url[1] = 'H') and 
+		(url[2] = 'T') and
+		(url[3] = 'T') and
+		(url[4] = 'P') and
+		(url[6] = ':') and
+		(url[7] = '/') and
+		(url[8] = '/');
+	end
+	else
+	begin
+		Result :=
+		(Length(url) > 7) and
+		(url[1] = 'H') and 
+		(url[2] = 'T') and
+		(url[3] = 'T') and
+		(url[4] = 'P') and
+		(url[5] = ':') and
+		(url[6] = '/') and
+		(url[7] = '/');
+	end;
+end;
