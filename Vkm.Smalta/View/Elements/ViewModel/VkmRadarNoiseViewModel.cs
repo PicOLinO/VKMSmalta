d8cfdbc9d9b43bb542b05f547f9fa09ef447f061
PicOLinO <webmaster.pico@gmail.com>
2018-10-09 15:38:32 +0300

    Создание элемента шумовой помехи

---
+++ Vkm.Smalta/View/Elements/ViewModel/VkmRadarNoiseViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Vkm.Smalta.View.Elements.ViewModel
+{
+    public class VkmRadarNoiseViewModel : ElementViewModelBase
+    {
+        public VkmRadarNoiseViewModel(int value, string name) : base(value, name)
+        {
+            OpacityPercents = value / 100d;
+        }
+
+        public double OpacityPercents
+        {
+            get { return GetProperty(() => OpacityPercents); }
+            set { SetProperty(() => OpacityPercents, value); }
+        }
+
+        public int RotationDegrees
+        {
+            get { return GetProperty(() => RotationDegrees); }
+            set { SetProperty(() => RotationDegrees, value); }
+        }
+
+        protected override void OnValueChanged()
+        {
+            base.OnValueChanged();
+
+            OpacityPercents = Value / 100d;
+        }
+    }
+}
---
+++ Vkm.Smalta/View/Elements/VkmRadarNoise.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Vkm.Smalta.View.Elements
+{
+    /// <summary>
+    /// Interaction logic for VkmRadarNoise.xaml
+    /// </summary>
+    public partial class VkmRadarNoise
+    {
+        public VkmRadarNoise()
+        {
+            InitializeComponent();
+        }
+    }
+}
