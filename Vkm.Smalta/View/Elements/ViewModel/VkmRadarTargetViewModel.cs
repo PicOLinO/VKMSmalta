5def8037839ff47de81366f0a6fc1c7062eeab3e
PicOLinO <webmaster.pico@gmail.com>
2018-10-09 14:00:20 +0300

    Добавление элемента отметки о цели

---
+++ Vkm.Smalta/View/Elements/ViewModel/VkmRadarTargetViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Vkm.Smalta.View.Elements.ViewModel
+{
+    public class VkmRadarTargetViewModel : ElementViewModelBase
+    {
+        protected VkmRadarTargetViewModel(int value, string name) : base(value, name)
+        {
+        }
+    }
+}
