f91aab3b49aaf3bc1fe8b1b946519169bb2c8c87
PicOLinO <webmaster.pico@gmail.com>
2018-10-16 15:01:14 +0300

    Зависимость значений

--- Vkm.Smalta/View/InnerPages/ViewModel/RlsOncInnerDevicePageViewModel.cs
+++ Vkm.Smalta/View/InnerPages/ViewModel/RlsOncInnerDevicePageViewModel.cs
@@ -35,22 +35,34 @@ namespace Vkm.Smalta.View.InnerPages.ViewModel
                 case RlsOncInnerRegionPage.G5_15:
                     Elements = new ObservableCollection<ElementViewModelBase>
                                {
-                                   GiveMe.Element().On(PageKey).WithName("g515_numberdisplay_timeshift").WithValue(10).At(246, 435).NumberDisplay().Please(),
-                                   GiveMe.Element().On(PageKey).WithName("g515_numberdisplay_repetition_rate").WithValue(0).At(246, 699).NumberDisplay().Please(),
-                                   GiveMe.Element().On(PageKey).WithName("g515_numberdisplay_amplitude_measurement").WithValue(0).At(246, 960).NumberDisplay().Please(),
+                                   GiveMe.Element().On(PageKey).WithName("g515_numberdisplay_timeshift").WithValue(5).At(246, 435).NumberDisplay().Please(),
+                                   GiveMe.Element().On(PageKey).WithName("g515_numberdisplay_repetition_rate").WithValue(400).At(246, 699).NumberDisplay().Please(),
+                                   GiveMe.Element().On(PageKey).WithName("g515_numberdisplay_amplitude_measurement").WithValue(10).At(246, 960).NumberDisplay().Please(),
 
                                    GiveMe.Element().On(PageKey).WithName("g515_stepwheel_timeshift").WithValue(0).At(91, 387).WithStartupRotation(60).RotateStepWheel().WithRotationStepDegrees(60).WithMaxValue(3).Please(),
                                    GiveMe.Element().On(PageKey).WithName("g515_stepwheel_duration").WithValue(0).At(98, 905).WithStartupRotation(-50).RotateStepWheel().WithRotationStepDegrees(31).WithMaxValue(10).Please(),
                                    
-                                   GiveMe.Element().On(PageKey).WithName("g515_wheel_timeshift").WithSize(50, 50).WithValue(10).At(327, 513).Wheel().WithImageType(ImageType.Flat).WithRotationCoefficient(20).WithMinValue(10).WithMaxValue(55)
+                                   GiveMe.Element().On(PageKey).WithName("g515_wheel_timeshift").WithSize(50, 50).WithValue(1).At(327, 513).Wheel().WithImageType(ImageType.Flat).WithRotationCoefficient(20).WithMinValue(1).WithMaxValue(11)
                                          .WithDependencyAction(GiveMe.DependencyAction()
                                                                      .WithDependencyElementName("g515_numberdisplay_timeshift")
                                                                      .TypeOf(DependencyType.CoefficientReplace)
-                                                                     .WithDependencyCoefficient(1)
+                                                                     .WithDependencyCoefficient(5)
                                                                      .Please())
                                          .Please(),
-                                   GiveMe.Element().On(PageKey).WithName("g515_wheel_repetition_rate").WithSize(50, 50).WithValue(0).At(327, 774).Wheel().WithImageType(ImageType.Flat).WithRotationCoefficient(20).WithMaxValue(40).Please(),
-                                   GiveMe.Element().On(PageKey).WithName("g515_wheel_amplitude_measurement").WithSize(50, 50).WithValue(0).At(327, 1036).Wheel().WithImageType(ImageType.Flat).WithRotationCoefficient(20).WithMaxValue(40).Please(),
+                                   GiveMe.Element().On(PageKey).WithName("g515_wheel_repetition_rate").WithSize(50, 50).WithValue(0).At(327, 774).Wheel().WithImageType(ImageType.Flat).WithRotationCoefficient(20).WithMinValue(8).WithMaxValue(20)
+                                         .WithDependencyAction(GiveMe.DependencyAction()
+                                                                     .WithDependencyElementName("g515_numberdisplay_repetition_rate")
+                                                                     .TypeOf(DependencyType.CoefficientReplace)
+                                                                     .WithDependencyCoefficient(50)
+                                                                     .Please())
+                                         .Please(),
+                                   GiveMe.Element().On(PageKey).WithName("g515_wheel_amplitude_measurement").WithSize(50, 50).WithValue(0).At(327, 1036).Wheel().WithImageType(ImageType.Flat).WithRotationCoefficient(20).WithMinValue(1).WithMaxValue(11)
+                                         .WithDependencyAction(GiveMe.DependencyAction()
+                                                                     .WithDependencyElementName("g515_numberdisplay_amplitude_measurement")
+                                                                     .TypeOf(DependencyType.CoefficientReplace)
+                                                                     .WithDependencyCoefficient(10)
+                                                                     .Please())
+                                         .Please(),
                                };
                     break;
                 case RlsOncInnerRegionPage.C1_65:
