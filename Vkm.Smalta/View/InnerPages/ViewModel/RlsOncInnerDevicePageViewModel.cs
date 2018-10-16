da536f25e8d770f2f6bddce54dd41444f38d36aa
PicOLinO <webmaster.pico@gmail.com>
2018-10-16 12:57:03 +0300

    Добавление элементов на страницы

--- Vkm.Smalta/View/InnerPages/ViewModel/RlsOncInnerDevicePageViewModel.cs
+++ Vkm.Smalta/View/InnerPages/ViewModel/RlsOncInnerDevicePageViewModel.cs
@@ -34,17 +34,29 @@ namespace Vkm.Smalta.View.InnerPages.ViewModel
                 case RlsOncInnerRegionPage.G5_15:
                     Elements = new ObservableCollection<ElementViewModelBase>
                                {
-                                   GiveMe.Element().On(PageKey).WithName("dummy_numberdisplay").WithValue(10).At(246, 704).NumberDisplay().Please()
+                                   GiveMe.Element().On(PageKey).WithName("dummy_numberdisplay1").WithValue(180).At(246, 446).NumberDisplay().Please(),
+                                   GiveMe.Element().On(PageKey).WithName("dummy_numberdisplay2").WithValue(180).At(246, 704).NumberDisplay().Please(),
+                                   GiveMe.Element().On(PageKey).WithName("dummy_numberdisplay3").WithValue(180).At(246, 953).NumberDisplay().Please(),
+
+                                   GiveMe.Element().On(PageKey).WithName("first_stepwheel_1").WithValue(0).At(91, 387).WithStartupRotation(60).RotateStepWheel().WithRotationStepDegrees(60).WithMaxValue(3).Please(),
+                                   GiveMe.Element().On(PageKey).WithName("first_stepwheel_2").WithValue(0).At(98, 905).WithStartupRotation(-50).RotateStepWheel().WithRotationStepDegrees(31).WithMaxValue(10).Please(),
+
+
+                                   GiveMe.Element().On(PageKey).WithName("dummy_wheel1").WithValue(0).At(327, 513).Wheel().WithImageType(ImageType.Flat).WithCoefficient(20).WithMaxValue(40).Please(),
+                                   GiveMe.Element().On(PageKey).WithName("dummy_wheel2").WithValue(0).At(327, 774).Wheel().WithImageType(ImageType.Flat).WithCoefficient(20).WithMaxValue(40).Please(),
+                                   GiveMe.Element().On(PageKey).WithName("dummy_wheel3").WithValue(0).At(327, 1036).Wheel().WithImageType(ImageType.Flat).WithCoefficient(20).WithMaxValue(40).Please(),
                                };
                     break;
                 case RlsOncInnerRegionPage.C1_65:
-                    Elements = new ObservableCollection<ElementViewModelBase>();
+                    Elements = new ObservableCollection<ElementViewModelBase>
+                               {
+                               };
                     break;
                 case RlsOncInnerRegionPage.Radar:
                     Elements = new ObservableCollection<ElementViewModelBase>
                                {
                                    GiveMe.Element().On(PageKey).WithName("dummy_target").WithValue(70).At(239, 649).RadarTarget().Please(),
-                                   GiveMe.Element().On(PageKey).WithName("dummy_noise").WithValue(70).WithStartupRotation(45).At(138, 603).RadarNoise().Please(),
+                                   GiveMe.Element().On(PageKey).WithName("dummy_noise").WithValue(70).WithStartupRotation(180).At(138, 603).RadarNoise().Please(),
                                };
                     break;
                 default:
