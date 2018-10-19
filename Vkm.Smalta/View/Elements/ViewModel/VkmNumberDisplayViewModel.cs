c46aca0c1e5355ff37ca46bc6a4989b49546d911
Данила Червонный <webmaster.pico@gmail.com>
2018-10-19 15:34:10 +0300

    Рефакторинг

--- Vkm.Smalta/View/Elements/ViewModel/VkmNumberDisplayViewModel.cs
+++ Vkm.Smalta/View/Elements/ViewModel/VkmNumberDisplayViewModel.cs
@@ -4,7 +4,6 @@ namespace Vkm.Smalta.View.Elements.ViewModel
 {
     public class VkmNumberDisplayViewModel : ElementViewModelBase
     {
-        //TODO: Implement
         public VkmNumberDisplayViewModel(int value, string name, int posTop, int posLeft, Enum page) : base(value, name, posTop, posLeft, page)
         {
         }
