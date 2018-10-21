a53397acfe43999d98be2748be65b372a2c931cb
Данила Червонный <webmaster.pico@gmail.com>
2018-10-22 00:28:00 +0300

    Добавление вкладок документации по устройствам

---
+++ Vkm.ComplexSim/Converters/TabItemWidthAdjustmentConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Data;
+using static System.Int32;
+
+namespace Vkm.ComplexSim.Converters
+{
+    public class TabItemWidthAdjustmentConverter : BaseConverter<TabItemWidthAdjustmentConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var lTabControlWidth = value as double? ?? 50;
+            var lTabsCount = (parameter is string s) ? Parse(s) : 1;
+            return lTabControlWidth / lTabsCount - 3;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
